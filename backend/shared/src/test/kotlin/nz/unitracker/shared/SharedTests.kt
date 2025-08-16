f26032f6d1701564ce5346d2d9ca55bc2985be03
Corey <cbh344@outlook.com>
2025-08-16 21:07:14 +1200

    add test for shared module

---
+++ backend/shared/src/test/kotlin/nz/unitracker/shared/SharedTests.kt
@@ -0,0 +1,12 @@
+package nz.unitracker.shared
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class SharedTests {
+
+    @Test
+    fun test() {
+        assertTrue { true }
+    }
+}
