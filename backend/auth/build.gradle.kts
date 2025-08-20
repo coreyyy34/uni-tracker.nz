74f02380f56166b4aa381f686d9f6aa17caf4308
Corey <cbh344@outlook.com>
2025-08-20 12:15:48 +1200

    add spring security and oauth2 starters

--- backend/auth/build.gradle.kts
+++ backend/auth/build.gradle.kts
@@ -2,4 +2,8 @@ version = "0.0.1-SNAPSHOT"
 
 dependencies {
     implementation(project(":shared"))
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 }
