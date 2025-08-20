d45463939dbf10b2e0f1bc099c43930327dd2262
Corey <cbh344@outlook.com>
2025-08-20 19:54:08 +1200

    add spring starter oauth2 jose to auth module

--- backend/auth/build.gradle.kts
+++ backend/auth/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-security-oauth2-jose")
 }
