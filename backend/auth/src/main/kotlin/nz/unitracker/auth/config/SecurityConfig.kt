35754930c79a38e6738747e046231eba1c0d546f
Corey Hines <cbh344@outlook.com>
2025-08-24 12:49:12 +1200

    define jwt encoder and decoder beans

--- backend/auth/src/main/kotlin/nz/unitracker/auth/config/SecurityConfig.kt
+++ backend/auth/src/main/kotlin/nz/unitracker/auth/config/SecurityConfig.kt
@@ -1,17 +1,27 @@
 package nz.unitracker.auth.config
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret
+import com.nimbusds.jose.proc.SecurityContext
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtEncoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder
 import org.springframework.security.web.SecurityFilterChain
+import javax.crypto.spec.SecretKeySpec
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
     private val authenticationSuccessHandler: OAuth2AuthenticationSuccessHandler,
+    @param:Value($$"${jwt.secret}") private val jwtSecret: String
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -34,5 +44,20 @@ class SecurityConfig(
                 csrf.ignoringRequestMatchers("/h2-console/**")
             }
         return http.build()
+    }
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        val secretKey = SecretKeySpec(jwtSecret.toByteArray(), "HmacSHA256")
+        return NimbusJwtDecoder.withSecretKey(secretKey)
+            .macAlgorithm(MacAlgorithm.HS256)
+            .build()
+    }
+
+    @Bean
+    fun jwtEncoder(): JwtEncoder {
+        val secretKey = SecretKeySpec(jwtSecret.toByteArray(), "HmacSHA256")
+        val jwk = ImmutableSecret<SecurityContext>(secretKey)
+        return NimbusJwtEncoder(jwk)
     }
 }
